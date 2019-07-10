78f3940633d35e52b6ba09525a1b07113cd0bd1b
fejkens <lol85251@gmail.com>
2019-07-10 20:41:00 +0100

    Todo items are now objects, toggleCompleted function added

--- scripts.js
+++ scripts.js
@@ -1,18 +1,29 @@
 const todos = {
-    todos: [],
-    displayTodos: function() {
-        console.log(this.todos);
-    },
-    addTodo: function(todo) {
-        this.todos.push(todo);
-        this.displayTodos();
-    },
-    changeTodo: function(index, newTodo) {
-        this.todos[index] = newTodo;
-        this.displayTodos();
-    },
-    deleteTodo: function(index) {
-        this.todos.splice(index, 1);
-        this.displayTodos();
-    }
+    todos: []
+}
+
+todos.displayTodos = function() {
+    console.log(this.todos);
+}
+
+todos.addTodo = function(text) {
+    this.todos.push({
+        todoText: text,
+        completed: false
+    });
+    this.displayTodos();
+}
+
+todos.changeTodo = function(index, newText) {
+    this.todos[index].todoText = newText;
+    this.displayTodos();
+}
+
+todos.deleteTodo = function(index) {
+    this.todos.splice(index, 1);
+    this.displayTodos();
+}
+
+todos.toggleCompleted = function(index) {
+    this.todos[index].completed = !this.todos[index].completed;
 }
