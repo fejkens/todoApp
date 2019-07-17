69e56f045ba889c01413254a309bd8a07fa25b5e
fejkens <lol85251@gmail.com>
2019-07-17 21:08:56 +0100

    Improved readability

--- scripts.js
+++ scripts.js
@@ -1,29 +1,38 @@
-if (JSON.parse(localStorage.getItem("todos")).length === 0) {
-    localStorage.setItem("todos", JSON.stringify([]));
-}
-
-const todos = {
-    todos: function() {
-        return JSON.parse(localStorage.getItem("todos"));
-    }
-};
-
+// jQuery style selector function
 let $ = function(elem) {
     return document.querySelector(elem);
 };
 
+// If todos array does not exist in localStorage, initiate it
+if (localStorage.getItem("todos") === null) {
+    localStorage.setItem("todos", JSON.stringify([]));
+}
+
+// Toggle all button event handler
 $("#toggleAll").onclick = () => { todos.toggleAll(); };
+
+// New todo button event handler
 $("#newTodoButton").onclick = () => {
     let newTodo = $("#newTodo").value;
     todos.addTodo(newTodo);
     $("#newTodo").value = "";
 };
 
+// Main todos object
+const todos = {};
+
+// Get todos from localStorage
+todos.todos = function() {
+    return JSON.parse(localStorage.getItem("todos"));
+}
+
+// Update todos stored in localStorage with a new array
 todos.updateTodos = function(arr) {
     arr = JSON.stringify(arr);
     localStorage.setItem("todos", arr);
 };
 
+// Add a todo
 todos.addTodo = function(text) {
     let todosArr = todos.todos();
     todosArr.push({
@@ -34,6 +43,7 @@ todos.addTodo = function(text) {
     display.displayTodos();
 };
 
+// Change a todo at index
 todos.changeTodo = function(index, newText) {
     let todosArr = todos.todos();
     todosArr[index].todoText = newText;
@@ -41,6 +51,7 @@ todos.changeTodo = function(index, newText) {
     display.displayTodos();
 };
 
+// Delete a todo
 todos.deleteTodo = function(index) {
     let todosArr = todos.todos();
     todosArr.splice(index, 1);
@@ -48,6 +59,7 @@ todos.deleteTodo = function(index) {
     display.displayTodos();
 };
 
+// Toggle the completed attribute
 todos.toggleCompleted = function(index) {
     let todosArr = todos.todos();
     todosArr[index].completed = !todosArr[index].completed;
@@ -55,6 +67,7 @@ todos.toggleCompleted = function(index) {
     display.displayTodos();
 };
 
+// Toggle the completed attribute for all todos
 todos.toggleAll = function() {
     let todosArr = todos.todos();
     let falseCheck = todosArr.filter((item) => {
@@ -74,34 +87,42 @@ todos.toggleAll = function() {
     display.displayTodos();
 };
 
+// Display object
 const display = {};
 
+// Display todos on the page
 display.displayTodos = function() {
+
     let todosList = $("#todosList");
+
+    // Clear current list before refreshing the display
     todosList.innerHTML = "";
     let todosArr = todos.todos();
+
     todosArr.forEach(function (elem, i) {
+        // Create todo item elements
         let todosLi = document.createElement("li");
         let todosCheckbox = document.createElement("input");
         let todoText = document.createElement("p");
         let editText = document.createElement("input");
 
-        todoText.innerHTML = elem.todoText;
-        todosCheckbox.type = "checkbox";
+        // Give each li item an id with the index of the item from the todo array
         todosLi.id = "todoId" + i;
-        todoText.className = "pFalse";
-        editText.type = "text";
-        editText.style.background = "transparent";
-        editText.style.border = "none";
-        editText.style.borderBottom = "solid 1px black";
-        editText.className = "editTodo";
-        editText.value = elem.todoText;
-        editText.autofocus = "true";
+
+        todosCheckbox.type = "checkbox";
+
+        todoText.innerHTML = elem.todoText;
+        todoText.className = "pFalse"; // Styling
 
         if (elem.completed === true) {
-            todoText.className = "pTrue";
+            todoText.className = "pTrue"; // Styling
         }
 
+        editText.type = "text";
+        editText.className = "editTodo";
+        editText.value = elem.todoText;
+
+        // Event listener for when checkboxes are ticked/unticked
         todosCheckbox.addEventListener("change", function() {
             if (todosCheckbox.checked === true) {
                 todosArr[i].completed = true;
@@ -112,34 +133,38 @@ display.displayTodos = function() {
             display.displayTodos();
         });
 
+        // When user clicks on the todo text, display the edit box
+        todoText.addEventListener("click", function() {
+            todoText.style.display = "none";
+            editText.style.display = "inline";
+            editText.focus();
+        });
+
+        // When user clicks away from the edit todo box save the text as the new todo text
         editText.addEventListener("blur", function() {
-            
             let newTodoText = editText.value;
             todos.changeTodo(i, newTodoText);
             todoText.style.display = "inline";
             editText.style.display = "none";
         });
 
-        todoText.addEventListener("click", function() {
-            todoText.style.display = "none";
-            editText.style.display = "inline";
-        });
-
-        todosLi.appendChild(todosCheckbox);
-        todosLi.appendChild(editText);
-        todosLi.appendChild(todoText);
-
+        // If item is marked as completed, update the checkbox
         if (elem.completed) {
             todosCheckbox.checked = true;
         } else {
             todosCheckbox.checked = false;
         }
 
+        // Append all items to the list
+        todosLi.appendChild(todosCheckbox);
+        todosLi.appendChild(editText);
+        todosLi.appendChild(todoText);
         todosLi.appendChild(display.createDeleteButton());
         todosList.appendChild(todosLi);
     });
 };
 
+// Create delete buttons for each todo displayed
 display.createDeleteButton = function() {
     let deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete";
@@ -154,4 +179,5 @@ display.createDeleteButton = function() {
     return deleteButton;
 };
 
+// Call the display function
 display.displayTodos();
